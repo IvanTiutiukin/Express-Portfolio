7ddf427d5ca972835c7e55620edbacb917467534
Ivan Tiutiukin <tyutyushkin71@gmail.com>
2022-06-21 22:45:45 -0400

    trying contacts

---
+++ server/Controllers/contact-list.ts
@@ -0,0 +1,113 @@
+import express from 'express';
+import { CallbackError } from 'mongoose';
+
+// import the Movie Model
+import Movie from '../Models/contact';
+
+import { UserDisplayName  } from '../Util';
+
+export function DisplayContactListPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+    Contact.find(function(err, contactsCollection)
+    {
+      // Database error
+      if(err)
+      {
+        console.error(err.message);
+        res.end(err);
+      }
+      res.render('index', { title: 'Contact List', page: 'contact-list', contacts: contactsCollection, displayName:  UserDisplayName(req)  });
+    });
+}
+
+export function DisplayAddPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+  res.render('index', { title: 'Add', page: 'edit', movie: '', displayName:  UserDisplayName(req) })
+}
+
+export function DisplayEditPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+  let id = req.params.id;
+
+  // pass the id to the db and read the movie into the edit page
+  Contact.findById(id, {}, {}, function(err, contactToEdit)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // show the edit view with the data
+    res.render('index', { title: 'Edit', page: 'edit', contact: contactToEdit, displayName:  UserDisplayName(req) })
+  });
+}
+
+export function ProcessAddPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+  // instantiate a new Movie to Add
+  let newContact = new Contact
+  ({
+    "Name": req.body.contactName,
+    "Email": req.body.contactEmail,
+    "Number": req.body.contactNumber
+  });
+
+  // Insert the new Movie object into the database (movies collection)
+  Contact.create(newContact, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // new movie has been added -> refresh the movie-list
+    res.redirect('/contact-list');
+  })
+}
+
+export function ProcessEditPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+  let id = req.params.id;
+
+  // instantiate a new Movie to Edit
+  let updatedContact = new Movie
+  ({
+    "_id": id,
+    "Name": req.body.contactName,
+    "Email": req.body.contactEmail,
+    "Number": req.body.contactNumber
+  });
+
+  // update the movie in the database
+  Contact.updateOne({_id: id}, updatedContact, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // edit was successful -> go to the movie-list page
+    res.redirect('/contact-list');
+  });
+}
+
+export function ProcessDeletePage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+  let id = req.params.id;
+
+  // pass the id to the database and delete the movie
+  Contact.remove({_id: id}, function(err: CallbackError)
+  {
+    if(err)
+    {
+      console.error(err);
+      res.end(err);
+    }
+
+    // delete was successful
+    res.redirect('/contact-list');
+  });
+}
